d1f08c80b6a0e5bda46032122aec160cbf708852
Thomas Edvalson <machin3@gmail.com>
2024-07-17 20:20:44 -0400

    Compile lxml

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-apk add g++
-cp /usr/bin/g++ /host/output
+apk add -q musl-dev gcc g++ py3-pip cmake make curl autoconf bison ninja
+apk add -q python3-dev libxml2-dev libxslt-dev yaml
+export MAKEFLAGS="-j$(nproc)"
+pip wheel -v -w /host/output lxml
