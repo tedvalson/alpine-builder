1d47b007cad883c105b8b5f1317c8772f893a30e
Thomas Edvalson <machin3@gmail.com>
2024-07-19 16:12:51 -0400

    Build pandas

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-apk add -q musl-dev gcc g++ py3-pip cmake make curl autoconf bison ninja
-apk add -q python3-dev libxml2-dev libxslt-dev yaml
+apk add -q musl-dev gcc g++ py3-pip cmake make curl autoconf bison ninja cargo
+apk add -q python3-dev
 export MAKEFLAGS="-j$(nproc)"
-pip wheel -v -w /host/output lxml
+export USE_CUDA=0
+pip wheel -v -w /host/output pandas
