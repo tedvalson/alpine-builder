453e067ae60ac0c111a8960958175211b561a16b
Thomas Edvalson <machin3@gmail.com>
2024-07-20 14:28:46 -0400

    Built llama-cpp-python

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-apk add -q musl-dev gcc g++ py3-pip cmake make curl autoconf bison ninja cargo
+apk add -q musl-dev gcc g++ py3-pip cmake make curl autoconf bison ninja cargo meson
 apk add -q python3-dev
 export MAKEFLAGS="-j$(nproc)"
 export USE_CUDA=0
-pip wheel -v -w /host/output pandas
+export CMAKE_ARGS="-DGGML_LLAMAFILE=OFF"
+pip wheel -v -w /host/output llama-cpp-python
