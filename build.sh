a617b72dc86898531ea20f949bb6f6e669ebbe3f
Thomas Edvalson <machin3@gmail.com>
2024-10-26 16:24:50 -0400

    Build deps for eyecite

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ apk update
 apk add -q musl-dev gcc g++ clang18 llvm18 py3-pip cmake make curl autoconf bison ninja cargo meson git
 apk add -q python3-dev openssl-dev clang18-libclang linux-headers
 #apk add -q --no-cache libc6-compat pkgconfig dav1d libdav1d dav1d-dev npm gcc
-pip wheel -v -w /host/output --no-binary=grpcio grpcio
+pip wheel -v -w /host/output pyahocorasick fast-diff-match-patch
